b5b5df38e3f5c840005218117b93e6dad161022f
Yasiru-sahan <sahanyasiru460@gmail.com>
2026-04-30 14:39:27 +0530

    fix(Customer) : cascade added

--- Application_Backend/src/main/java/com/org/project/application/entity/Customer.java
+++ Application_Backend/src/main/java/com/org/project/application/entity/Customer.java
@@ -21,4 +21,7 @@ public class Customer {
 
     @Column(nullable = false)
     private String password;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Order> order;
 }
